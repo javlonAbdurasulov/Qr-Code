2a8df5bcb2c9f8e87857f2c95f32d980934cb0cf
abdurasulov2203 <abdurasulov2203@gmail.com>
2024-10-20 23:14:59 +0500

    Add project files.

---
+++ Qr-Code/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Qr_Code
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			components = new System.ComponentModel.Container();
+			comboBox1 = new ComboBox();
+			textBox1 = new TextBox();
+			pictureBox1 = new PictureBox();
+			button1 = new Button();
+			button2 = new Button();
+			timer1 = new System.Windows.Forms.Timer(components);
+			((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+			SuspendLayout();
+			// 
+			// comboBox1
+			// 
+			comboBox1.FormattingEnabled = true;
+			comboBox1.Location = new Point(133, 12);
+			comboBox1.Name = "comboBox1";
+			comboBox1.Size = new Size(509, 28);
+			comboBox1.TabIndex = 0;
+			// 
+			// textBox1
+			// 
+			textBox1.Location = new Point(66, 57);
+			textBox1.Name = "textBox1";
+			textBox1.Size = new Size(663, 27);
+			textBox1.TabIndex = 1;
+			// 
+			// pictureBox1
+			// 
+			pictureBox1.Location = new Point(66, 90);
+			pictureBox1.Name = "pictureBox1";
+			pictureBox1.Size = new Size(663, 338);
+			pictureBox1.TabIndex = 2;
+			pictureBox1.TabStop = false;
+			// 
+			// button1
+			// 
+			button1.Location = new Point(505, 434);
+			button1.Name = "button1";
+			button1.Size = new Size(93, 38);
+			button1.TabIndex = 3;
+			button1.Text = "button1";
+			button1.UseVisualStyleBackColor = true;
+			button1.Click += button1_Click;
+			// 
+			// button2
+			// 
+			button2.Location = new Point(635, 434);
+			button2.Name = "button2";
+			button2.Size = new Size(94, 38);
+			button2.TabIndex = 4;
+			button2.Text = "button2";
+			button2.UseVisualStyleBackColor = true;
+			button2.Click += button2_Click;
+			// 
+			// timer1
+			// 
+			timer1.Tick += timer1_Tick;
+			// 
+			// Form1
+			// 
+			AutoScaleDimensions = new SizeF(8F, 20F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(796, 488);
+			Controls.Add(button2);
+			Controls.Add(button1);
+			Controls.Add(pictureBox1);
+			Controls.Add(textBox1);
+			Controls.Add(comboBox1);
+			Name = "Form1";
+			Text = "Form1";
+			FormClosing += Form1_FormClosing;
+			Load += Form1_Load;
+			((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+			ResumeLayout(false);
+			PerformLayout();
+		}
+
+		#endregion
+
+		private ComboBox comboBox1;
+		private TextBox textBox1;
+		private PictureBox pictureBox1;
+		private Button button1;
+		private Button button2;
+		private System.Windows.Forms.Timer timer1;
+	}
+}
---
+++ Qr-Code/Form1.cs
@@ -0,0 +1,162 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using QRCoder;
+using ZXing;
+using ZXing.Common;
+using ZXing.QrCode;
+using ZXing.QrCode;
+
+
+namespace Qr_Code
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		private FilterInfoCollection CaptureDevice;
+		private VideoCaptureDevice FinalFrame;
+		private void FinalFrame_NewFrame(object sender, NewFrameEventArgs eventArgs)
+		{
+			pictureBox1.Image = (Image)eventArgs.Frame.Clone();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			CaptureDevice = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+			foreach (FilterInfo Device in CaptureDevice)
+			{
+				comboBox1.Items.Add(Device.Name);
+			}
+			comboBox1.SelectedIndex = 0;
+			FinalFrame = new VideoCaptureDevice();
+		}
+
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			if (FinalFrame.IsRunning == true)
+			{
+				FinalFrame.SignalToStop();
+				FinalFrame.WaitForStop();
+			}
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			FinalFrame = new VideoCaptureDevice(CaptureDevice[comboBox1.SelectedIndex].MonikerString);
+			FinalFrame.NewFrame += new NewFrameEventHandler(FinalFrame_NewFrame);
+			FinalFrame.Start();
+			button1.Enabled = false;
+			button2.Enabled = true;
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			timer1.Start();
+			button2.Enabled = false;
+			textBox1.Text = "";
+		}
+		private byte[] ConvertBitmapToByteArray(Bitmap bitmap)
+		{
+			using (MemoryStream stream = new MemoryStream())
+			{
+				bitmap.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+
+				return stream.ToArray();
+			}
+		}
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			Bitmap bitmap = new Bitmap(pictureBox1.Image);
+
+			BitmapLuminanceSource source = new BitmapLuminanceSource(bitmap);
+			BinaryBitmap binaryBitmap = new BinaryBitmap(new HybridBinarizer(source));
+			QRCodeReader qrCodeReader = new QRCodeReader();
+			
+			try
+			{
+				Result result = qrCodeReader.decode(binaryBitmap);
+				if (result != null)
+				{
+					string decodedData = result.Text;
+					textBox1.Text = decodedData;
+				}
+			}
+			catch (ReaderException rex)
+			{
+				Console.WriteLine("Error decoding QR Code: " + rex.Message);
+			}
+			#region MyRegion
+
+			//string imagePath = "путь_к_изображению";
+			////Image image = (Image)pictureBox1.Image;
+			////BinaryBitmap binary = (BinaryBitmap)image;
+			////QRCodeReader QrReader = new QRCodeReader();
+			////QRCodeData qrdata = QrReader.decode(binary);
+			//// Загрузите изображение
+			//var bitmap = (Bitmap)Image.FromFile(imagePath);
+			////ZXing.LuminanceSource luminanceSource = new ZXing.RGBLuminanceSource(bitmap, bitmap.Width, bitmap.Height);
+			////BinaryBitmap binaryBitmap = new BinaryBitmap(new HybridBinarizer(luminanceSource));
+
+			//// Прочтите QR-код с изображения
+			////var result = QrReader.decode();
+
+			//// Выведите результат
+			////if (result != null)
+			////{
+			////	Console.WriteLine("Содержимое QR-кода: " + result.Text);
+			////}
+			////else
+			////{
+			////	Console.WriteLine("QR-код не обнаружен.");
+			////}
+			//////BarcodeReader Reader = new BarcodeReader();
+			/////*using (*/
+			//////var ms = new MemoryStream();/*)*/
+			//////	//{
+
+			//////	pictureBox1.Image.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+			//////	byte[] bytes = ms.ToArray();
+			////Bitmap bitmap = new Bitmap(pictureBox1.Image);
+
+			//////ZXing.LuminanceSource luminanceSource = new ZXing.Windows.Compatibility.WindowsImageLuminanceSource(bitmap);
+			////ZXing.LuminanceSource luminanceSource = new ZXing.RGBLuminanceSource(bitmap, bitmap.Width, bitmap.Height);
+
+			//////LuminanceSource luminanceSource = new LuminanceSource(bitmap);
+			////Result result = Reader.Decode(luminanceSource);
+			////	//}
+
+			////try
+			////{
+			////	string decoded = result.ToString().Trim();
+			////	//ms.Close();
+			////	textBox1.Text = decoded;
+
+			////	if(decoded != "123")
+			////	{
+			////		timer1.Stop();
+			////		button2.Enabled=true;
+			////		MessageBox.Show("Welcome");
+			////		textBox1.Text=decoded;
+			////	}
+			////}
+			////catch(Exception ex)
+			////{
+			////         }
+
+			#endregion
+
+
+		}
+	}
+}
